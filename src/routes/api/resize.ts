2a6e5e01ee7a89b181203d5480a37fa1cc5fd19c
Erwin Rosales <look4erwin@gmail.com>
2022-02-21 00:57:38 -0800

    Added comments and ran sharp async

--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -8,34 +8,34 @@ import { RequestHandler } from '../../utilities/RequestHandler'
 const resize = express.Router();
 
 resize.get('/', async (req: express.Request, res: express.Response) => {
-
-
     try {
+        // Instantiate RequestHandler with queried args
         const myImage = new RequestHandler(
             req.query.title as string, 
             parseInt(`${req.query.width}`),
             parseInt(`${req.query.height}`)
-        );    
-
-        // Output consistent naming convention for processed images via Sharp
-        const outputImageName = `${myImage.title}_${myImage.width}x${myImage.height}.jpeg`;
-        // Path to output folder via Sharp's toFile() method
-        const outputPath = path.join(__dirname, '../../', 'public', 'output', outputImageName);
+        );   
         
-        // Output resized image
-        await sharp(myImage.inputPath())
-            .resize(myImage.width, myImage.height)
-            .jpeg({
-                quality: 50,
-                progressive: true
-            })
-            .toFile(outputPath)
+        // Create specifications for input and output for processed images
+        const inputPath = myImage.inputPath(); 
+        const outputPath = myImage.outputPath();
+        const outputImageName = myImage.outputImageName();
 
+        // Asynchronously run Sharp to process and output images
+        await sharp(inputPath)
+        .resize(myImage.width, myImage.height)
+        .jpeg({
+            quality: 50,
+            progressive: true
+        })
+        .toFile(outputPath);
+    
+        // Display processed image to browser
         res.send(`
             <h2>Image Processing API</h2>
             <img src="/output/${outputImageName}" />
         `)
-
+        
     } catch(err) {
         // Handle ERROR
         res.send(`${err}`);
--- src/utilities/RequestHandler.ts
+++ src/utilities/RequestHandler.ts
@@ -2,31 +2,6 @@ import { promises as fs } from 'fs'
 import path from 'path'
 import sharp from 'sharp'
 
-
-// export class RequestHandler {
-//     // Initialize properties
-//     constructor() {}
-
-//     // Input path to be used in Sharp
-//     inputPath(title: string) {
-//         return path.join(__dirname, '../', 'images', 'input', `${title}.jpg`);
-//     }
-
-//     // Output path and file output file renaming
-//     outputPath(title: string, width: number, height: number) {
-//         return path.join(__dirname, '../', 'images', 'output', `${title}_${width as number}x${height as number}.jpeg`);
-//     }  
-
-//     runSharp(title: string, width: number, height: number) {
-//         // TODO: (1) Get title from query (2) Search for the file in proper dir (3) Run Sharp        
-        
-
-//         sharp()
-//             .resize(width, height)
-//             .toFile(path.join(__dirname, '../../', 'public', 'output', `${title}_${width}x${height}.jpeg`))
-//     }
-// }
-
 export class RequestHandler {
     // Class properties
     title: string;
@@ -39,14 +14,20 @@ export class RequestHandler {
         this.height = height;
     }
 
-    //TODO: Create an input path for Sharp
+    // Input path for Sharp
     inputPath():string {
         const inputDir = path.join(__dirname, '../', 'public', 'input', `${this.title}.jpg`);
         // const inputFile = inputDir.split('/')[8];
         return inputDir;
     }
-    
-    //TODO: Create an output path for Sharp
-    //TODO: Serve Sharp to the client
+
+    outputImageName():string {
+        return `${this.title}_${this.width}x${this.height}.jpeg`;
+    }
+
+    // Path to output folder via Sharp's toFile() method
+    outputPath():string { 
+        return path.join(__dirname, '../', 'public', 'output', this.outputImageName());
+    }
     
 }
